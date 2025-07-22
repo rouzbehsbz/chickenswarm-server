74707561f551bfc11df2da9203f398a980a0a220
rouzbeh sabzehei <rouzbehsbz>
2025-07-23 00:01:32 +0330

    fix: remove config reader

--- cmd/main.go
+++ cmd/main.go
@@ -12,12 +12,12 @@ func main() {
 	isDevMode := flag.Bool("dev", true, "Run program in dev mode")
 	flag.Parse()
 
-	config, err := common.NewConfig(*isDevMode)
+	_, err := common.NewConfig(*isDevMode)
 	if err != nil {
 		panic(err)
 	}
 
-	address := fmt.Sprintf("%s:%d", config.Host, config.Port)
+	address := fmt.Sprintf("%s:%d", "0.0.0.0", 3001)
 	game := engine.NewGame()
 
 	http.HandleFunc("/ws", game.Server.UpgradeConnection)
